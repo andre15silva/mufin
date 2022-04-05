042354422cc100f019c22695f1668206a28e722e
André Silva <andre15andre@hotmail.com>
2022-04-05 16:48:45 +0100

    Update filter_test
    Signed-off-by: André Silva <andre15andre@hotmail.com>

--- src/filter_test.py
+++ src/filter_test.py
@@ -16,17 +16,24 @@ if __name__ == "__main__":
     # Check bugs patches
     to_remove = set()
     for bug in dataset.get_bugs():
-        test_bug = bug.test()
-        test_fixed = bug.test_fixed()
-        if not test_fixed.is_executing() or not test_fixed.is_executing():
-            print("Bug %s tests didn't execute." % bug.get_identifier())
-            to_remove.add(bug)
-        elif test_bug.is_passing():
-            print("Bug %s is buggy but the tests pass." % bug.get_identifier())
-            to_remove.add(bug)
-        elif not test_fixed.is_passing():
-            print("Bug %s is fixed but the tests fail." % bug.get_identifier())
-            to_remove.add(bug)
+        # We only want to check the fixed version because it has no diff
+        if not bug.get_diff():
+            test_fixed = bug.test_fixed()
+            if not test_fixed.is_executing() or not test_fixed.is_executing():
+                print("Bug %s tests didn't execute." % bug.get_identifier())
+                to_remove.add(bug)
+        else:
+            test_bug = bug.test()
+            test_fixed = bug.test_fixed()
+            if not test_fixed.is_executing() or not test_fixed.is_executing():
+                print("Bug %s tests didn't execute." % bug.get_identifier())
+                to_remove.add(bug)
+            elif test_bug.is_passing():
+                print("Bug %s is buggy but the tests pass." % bug.get_identifier())
+                to_remove.add(bug)
+            elif not test_fixed.is_passing():
+                print("Bug %s is fixed but the tests fail." % bug.get_identifier())
+                to_remove.add(bug)
 
     # Remove bugs that don't execute the tests
     for bug in to_remove:
