5338455f235cbedcdbe3322d7f1bcfc8ef70cbae
André Silva <andre15andre@hotmail.com>
2022-04-07 14:14:31 +0100

    Implement generate bugs script

--- src/generate_bugs.py
+++ src/generate_bugs.py
@@ -2,8 +2,139 @@ import argparse
 import sys
 import pathlib
 import subprocess
+import os
+import tempfile
+import shutil
+import uuid
 
 import utils
+from models.bug import Bug
+from models.defects4j.defects4jbug import Defects4JBug
+from models.bugsdotjar.bugsdotjar import BugsDotJarBug
+from models.bears.bearsbug import BearsBug
+from models.quixbugs.quixbugsbug import QuixBugsBug
+
+
+def apply_bug(original_file, infos):
+    perturbed_file = tempfile.NamedTemporaryFile(mode="w+", encoding="utf-8", delete=False, suffix=".java")
+
+    action = infos[0]
+    curruptCode =  infos[1]  
+    lineNo1 =  infos[2]
+    lineNo2 =  infos[3]
+    lineNo3 =  infos[4]
+    lineNo4 =  infos[5]
+    lineNo5 =  infos[6]
+
+    # read and perturb code 
+    perturbStr = ""
+    if "ADD" in action or "REPLACE" in action:
+        with open(original_file, "r") as f:
+            lines = f.readlines()
+            for i in range(0,len(lines)):
+                if i+1< int(lineNo1) or i+1> int(lineNo1)+4:
+                    perturbStr+=lines[i]
+                elif i+1==int(lineNo1):
+                    perturbStr+=curruptCode+"\n"
+                elif i+1==int(lineNo1)+1: 
+                    if lineNo2=='':
+                        perturbStr+=lines[i]
+                    else:
+                        perturbStr+=" \n"
+                elif i+1==int(lineNo1)+2:
+                    if lineNo3=='':
+                        perturbStr+=lines[i]
+                    else:
+                        perturbStr+=" \n"
+                elif i+1==int(lineNo1)+3:  
+                    if lineNo4=='':
+                        perturbStr+=lines[i]
+                    else:
+                        perturbStr+=" \n"
+                elif i+1==int(lineNo1)+4:
+                    if lineNo5=='':
+                        perturbStr+=lines[i]
+                    else:
+                        perturbStr+=" \n"
+    elif "REMOVE" in action :
+        with open(original_file, "r") as f:
+            lines = f.readlines()
+            for i in range(0,len(lines)):
+                if i+1< int(lineNo1) or i+1> int(lineNo1)+4:
+                    perturbStr+=lines[i]
+                elif i+1==int(lineNo1):
+                    perturbStr+= lines[i]+" \n" +curruptCode
+                elif i+1==int(lineNo1)+1: 
+                    if lineNo2=='':
+                        perturbStr+=lines[i]
+                    else:
+                        perturbStr+=" \n"
+                elif i+1==int(lineNo1)+2:
+                    if lineNo3=='':
+                        perturbStr+=lines[i]
+                    else:
+                        perturbStr+=" \n"
+                elif i+1==int(lineNo1)+3:  
+                    if lineNo4=='':
+                        perturbStr+=lines[i]
+                    else:
+                        perturbStr+=" \n"
+                elif i+1==int(lineNo1)+4:
+                    if lineNo5=='':
+                        perturbStr+=lines[i]
+                    else:
+                        perturbStr+=" \n"
+
+    # Write the perturbed code back to the tmp file
+    perturbed_file.write(perturbStr)
+    perturbed_file.close()
+    
+    diff = utils.get_diff(original_file, perturbed_file)
+    os.remove(str(pathlib.Path(perturbed_file.name).absolute()))
+    return diff
+
+
+def create_bug(args, original_bug, diff) -> Bug:
+    uid = str(uuid.uuid4())
+    if args.defects4j != None:
+        return Defects4JBug(original_bug.get_identifier() + "-" + uid, original_bug.get_path(), diff)
+    elif args.bugsdotjar != None:
+        return BugsDotJarBug(original_bug.get_identifier() + "-" + uid, original_bug.get_path(), diff)
+    elif args.bears != None:
+        return BearsBug(original_bug.get_identifier() + "-" + uid, original_bug.get_path(), diff)
+    elif args.quixbugs != None:
+        return QuixBugsBug(original_bug.get_identifier() + "-" + uid, original_bug.get_path(), diff)
+    else:
+        return NotImplementedError("%s" % args)
+
+
+def construct_bug(args, original_bug, original_file, perturbations_file):
+    bugs = []
+    if pathlib.Path(perturbations_file).exists():
+        with open(perturbations_file, "r") as pf:
+            for line in pf.readlines():
+                try:
+                    if not '^' in line: continue
+                    infos = line.split('^')
+                    if len(infos) != 11: continue
+
+                    # Create bug
+                    diff = apply_bug(original_file, infos)
+                    if diff != "":
+                        bug = create_bug(args, original_bug, diff)
+                        bugs.append(bug)
+
+                except Exception as e:
+                    print(e)
+                    continue
+            
+                # TODO: Debug purposes only
+                #break
+    
+        os.remove(perturbations_file)
+
+    return bugs
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Script to generate bugs for all projects of a dataset")
@@ -17,6 +148,7 @@ if __name__ == "__main__":
         print("The perturbation_model option must be set.")
         sys.exit(1)
 
+    bugs_to_add = []
     for bug in dataset.get_bugs():
         for file in pathlib.Path(bug.get_path()).glob("**/*.java"):
             # We don't want to generate bugs on the tests
@@ -24,7 +156,18 @@ if __name__ == "__main__":
             if "test" not in relative_path and "Test" not in relative_path:
                 cmd = "timeout 600 java -jar %s %s" % (args.perturbation_model, file)
                 run = subprocess.run(cmd, shell=True, capture_output=True)
-                sys.exit(0)
 
+                generated_bug = construct_bug(args, bug, file, "perturbationFile.txt")
+                if generated_bug != None:
+                    bugs_to_add.extend(generated_bug)
+
+            # TODO: debug purposes only
+            #break
         # TODO: debug purposes only
-        break
+        #break
+
+    for bug in bugs_to_add:
+        dataset.add_bug(bug)
+
+    # Save the metadata
+    utils.save_dataset(args, dataset)
