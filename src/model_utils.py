65dd6f84ddefc89b2ec5bf1943c6fad1c8aec551
André Silva <andre15andre@hotmail.com>
2022-05-06 17:59:28 +0100

    update tokenization

--- src/model_utils.py
+++ src/model_utils.py
@@ -3,7 +3,7 @@ from unidiff import PatchSet
 # TODO: Change this to generate the definitive complex training sample
 def source_str(example):
     diff = PatchSet(example)
-    for line in diff[0][0].source_lines():
+    for line in diff[0][0].target_lines():
         if line.is_removed:
             return line.value.strip()
 
@@ -11,6 +11,6 @@ def source_str(example):
 # TODO: Change this to generate the definitive complex training sample
 def target_str(example):
     diff = PatchSet(example)
-    for line in diff[0][0].target_lines():
+    for line in diff[0][0].source_lines():
         if line.is_added:
             return line.value.strip() 
--- src/utils.py
+++ src/utils.py
@@ -8,7 +8,7 @@ from serialization.decoders import DatasetDecoder
 
 
 def get_diff(source: pathlib.Path, target: pathlib.Path) -> str:
-    cmd = "find {} {} -type f ! -name '*.java' -printf '%f\n' | diff -N -u -r {} {} -X -".format(source, target, source, target)
+    cmd = "find {} {} -type f ! -name '*.java' -printf '%f\n' | diff -N -U 5 -r {} {} -X -".format(source, target, source, target)
     run = subprocess.run(cmd, shell=True, capture_output=True)
     return run.stdout.decode("utf-8", errors="ignore")
 
