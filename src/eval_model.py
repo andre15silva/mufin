fc2830b89b8314bd6d83327526d6b86e95276b40
André Silva <andre15andre@hotmail.com>
2022-06-07 17:08:24 +0100

    update eval model

--- src/eval_model.py
+++ src/eval_model.py
@@ -16,8 +16,7 @@ from models.compile_result import CompileResult
 from models.test_result import TestResult
 from models.bug import Bug
 from models.defects4j.defects4jbug import Defects4JBug
-from models.bugsdotjar.bugsdotjar import BugsDotJarBug
-from models.bears.bearsbug import BearsBug
+from models.bugsdotjar.bugsdotjar import BugsDotJarBug from models.bears.bearsbug import BearsBug
 from models.quixbugs.quixbugsbug import QuixBugsBug
 
 
@@ -39,19 +38,34 @@ def extract_ground_truth(bug):
     # Parse the diff and access info
     diff = PatchSet(bug.get_diff())
     
-    buggy_line = ""
-    for line in diff[0][0].target_lines():
+    start_buggy = -1
+    end_buggy = -1
+    for i, line in enumerate(diff[0][0].target_lines()):
         if line.is_added:
-            buggy_line = line.value.strip()
-            break
+            if start_buggy == -1:
+                start_buggy = i
+            if end_buggy < i:
+                end_buggy = i
+    start_fix = -1
+    end_fix = -1
+    for i, line in enumerate(diff[0][0].source_lines()):
+        if line.is_removed:
+            if start_fix == -1:
+                start_fix = i
+            if end_fix < i:
+                end_fix = i
+    
+    buggy_line = ""
+    for i, line in enumerate(diff[0][0].target_lines()):
+        if i >= start_buggy and i <= end_buggy:
+            buggy_line += " " + line.value.strip() + " "
 
     fixed_line = ""
     for line in diff[0][0].source_lines():
-        if line.is_removed:
-            fixed_line = line.value.strip()
-            break
+        if i >= start_fix and i <= end_fix:
+            fixed_line += " " + line.value.strip() + " "
 
-    return buggy_line, fixed_line
+    return " ".join(buggy_line.split()), " ".join(fixed_line.split())
 
 
 def apply_fix(original_bug, tentative_fix):
