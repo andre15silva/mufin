8a806ab356e659ae390744db25efd627c276f094
André Silva <andre15andre@hotmail.com>
2022-06-07 16:37:42 +0100

    update source and target representations

--- run_data_collection.sh
+++ run_data_collection.sh
@@ -16,6 +16,6 @@ if [[ ! " ${!datasets[*]} " =~ " $2 " ]]; then
 fi
 
 python src/checkout.py --storage $1 --$2 ${datasets[$2]} > $1/$2_checkout.out 2>&1
-python src/filter_single_hunk.py --storage $1 --$2 ${datasets[$2]} --model_output $2_hunk.json --keep_single_line_only > $1/$2_hunk.out 2>&1
+python src/filter_single_hunk.py --storage $1 --$2 ${datasets[$2]} --model_output $2_hunk.json > $1/$2_hunk.out 2>&1
 python src/filter_compile.py --storage $1 --$2 ${datasets[$2]} --model_input $2_hunk.json --model_output $2_hunk_compile.json > $1/$2_compile.out 2>&1
 python src/filter_test.py --storage $1 --$2 ${datasets[$2]} --model_input $2_hunk_compile.json --model_output $2_hunk_compile_test.json > $1/$2_test.out 2>&1
--- src/model_utils.py
+++ src/model_utils.py
@@ -1,22 +1,41 @@
 from unidiff import PatchSet
 
-# TODO: Change this to generate the definitive complex training sample
-# It should iterate both target and source lines and add the buggy bounds
+
 def source_str(example):
     diff = PatchSet(example)
     source = ""
-    for line in diff[0][0].target_lines():
+    start_buggy = -1
+    end_buggy = -1
+    for i, line in enumerate(diff[0][0].target_lines()):
         if line.is_added:
-            source += " [START_BUGGY] " + line.value.strip() + " [END_BUGGY] "
-        else:
-            source += line.value.strip()
+            if start_buggy == -1:
+                start_buggy = i
+            if end_buggy < i:
+                end_buggy = i
+    
+    for i, line in enumerate(diff[0][0].target_lines()):
+        if i == start_buggy:
+            source += " [START_BUGGY] "
+        source += " " + line.value.strip() + " "
+        if i == end_buggy:
+            source += " [END_BUGGY] "
+
     return source
 
 
-# TODO: Change this to generate the definitive complex training sample
 def target_str(example):
     diff = PatchSet(example)
-    for line in diff[0][0].source_lines():
+    start_fix = -1
+    end_fix = -1
+    for i, line in enumerate(diff[0][0].source_lines()):
         if line.is_removed:
-            return line.value.strip()
-    return ""
+            if start_fix == -1:
+                start_fix = i
+            if end_fix < i:
+                end_fix = i
+
+    target = ""
+    for i, line in enumerate(diff[0][0].source_lines()):
+        if i >= start_fix and i <= end_fix:
+            target += " " + line.value.strip() + " "
+    return target
