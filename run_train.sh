a691bbf03cd1e3cd0fc7fcf5af5ec0d9253f42a9
André Silva <andre15andre@hotmail.com>
2022-07-04 10:23:19 +0100

    update training script

--- run_train.sh
+++ run_train.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-if [ $# != 4 ]; then
-    echo "Usage: ./run_train.sh <model_storage> <direction> <training_dataset> <validation_dataset>"
+if [ $# != 4 ] && [ $# != 5 ]; then
+    echo "Usage: ./run_train.sh <model_storage> <direction> <training_dataset> <validation_dataset> [<path_to_pretrained_model>]"
     exit 1
 fi
 
-python src/train_model.py --model_storage $1 --$2 --training_dataset $3 --validation_dataset $4 --max_epochs 2 --samples_per_epoch 30000000 > $1/training_logs.out 2>&1
+if [ $# == 4 ]; then
+    echo "FOUR"
+    python src/train_model.py --model_storage $1 --$2 --training_dataset $3 --validation_dataset $4 --max_epochs 2 --samples_per_epoch 30000000 > $1/training_logs.out 2>&1
+fi
+
+if [ $# == 5 ]; then
+    echo "FIVE"
+    python src/train_model.py --model_storage $1 --$2 --training_dataset $3 --validation_dataset $4 --max_epochs 2 --samples_per_epoch 30000000 --from_pretrained $5 > $1/training_logs.out 2>&1
+fi
