fff4f859fc19bad95464f610fdfbd65fdb0fd973
André Silva <andre15andre@hotmail.com>
2022-09-26 18:54:16 +0100

    update stats for bears

--- src/stats.py
+++ src/stats.py
@@ -2,6 +2,9 @@ import argparse
 import sys
 import pandas as pd
 import pathlib
+import subprocess
+import json
+from datetime import datetime
 
 import utils
 import serialization_utils
@@ -15,10 +18,36 @@ if __name__ == "__main__":
     dataset = serialization_utils.load_dataset(args)
 
     # Compute stats
-    key = utils.get_json_input_file(args).stem
-    data = {key : {}}
-    data[key]["n_bugs"] = len(dataset.get_bugs())
+    data = []
+    for bug in dataset.get_bugs():
+        bug_info = { 
+                "identifier" : bug.get_identifier(),
+                "project" : "",
+                "commitid" : "",
+                "date" : "",
+                "loc" : "",
+                "tests" : ""
+                }
+
+        # Read bears.json
+        with open("/home/andre/Repos/mscthesis/storage/bears/Bears-1/bears.json", "r") as f:
+            info = json.load(f)
+            bug_info["project"] = info["repository"]["name"]
+            bug_info["commitid"] = info["commits"]["fixerBuild"]["sha"][:6]
+
+            date = datetime.strptime(info["commits"]["fixerBuild"]["date"], "%b %d, %Y %I:%M:%S %p")
+            bug_info["date"] = date.strftime("%Y-%m-%d")
+
+            cmd = "cd %s; find . -name '*.java' -and -not -path '*test*' -and -not -path '*Test*' | xargs cat | sed '/^\s*$/d' | wc -l"
+            run = subprocess.run(cmd, shell=True, capture_output=True)
+            bug_info["loc"] = int(run.stdout.decode("utf-8"))
+            bug_info["tests"] = info["tests"]["overallMetrics"]["numberRunning"]
+
+        data.append(bug_info)
 
     # Save stats to csv file
-    df = pd.DataFrame.from_dict(data, orient="index")
+    df = pd.DataFrame(data)
+    df = df.set_index("identifier")
+    df = df.sort_index()
+    key = serialization_utils.get_json_input_file(args).stem
     df.to_csv(pathlib.Path(args.storage, key + ".csv"))
